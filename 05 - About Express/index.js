ae70a6bf907349d898b1e244386c24b3d6c2eedb
Codearick <rohan.gautam650@gmail.com>
2024-04-25 14:42:56 +0530

    add update

--- 05 - About Express/index.js
+++ 05 - About Express/index.js
@@ -5,9 +5,15 @@ const app = express();
 const port = 8000;
 
 app.get("/", (req, res) => {
-    res.send(html, <h1>On slash "/" route</h1>)
+    res.send(`<h1>On slash "/" route</h1>`)
 })
 
 app.listen(port, () => {
     console.log("app listening on port ",port)
+})
+
+app.use((req, res) => {
+    console.log("Request received!")
+    console.log("This is request: ",req);
+    console.log("This is response: ",res);
 })
