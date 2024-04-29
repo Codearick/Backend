0af40800b84242e25056cdde5183da2c15e6cdf1
Codearick <rohan.gautam650@gmail.com>
2024-04-30 02:37:48 +0530

    add index file code changes

--- 05 - About Express/index.js
+++ 05 - About Express/index.js
@@ -8,6 +8,24 @@ app.get("/", (req, res) => {
     res.send(`<h1>On slash "/" route</h1>`)
 })
 
+app.get("/:username/:id", (req, res) => {
+    console.log(req.params)
+    res.send(`Hello this is params: ${req.params}`)
+})
+
+app.get("/search", (req, res) => {
+    const {q} = req.query
+    if(!q) {
+        res.send("Nothing searched!")
+    }
+    res.send(`<h1>Searched : ${q}</h1>`)
+})
+
+app.get("*", (req, res) => {
+    res.send(`This path doesn't exist!`)
+})
+
+
 app.listen(port, () => {
     console.log("app listening on port ",port)
 })
